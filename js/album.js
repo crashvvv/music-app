9765d3bb47d9de461b53868ee72fda7225fc8ece
crashvvv <crash16.10.85@gmail.com>
2023-02-25 10:07:58 +0300

    modified:   js/album.js

--- js/album.js
+++ js/album.js
@@ -36,12 +36,12 @@ if (!album) {
         playlist.innerHTML += `
         <li class="track list-group-item d-flex align-items-center">
             <img src="assets/play.svg" alt="" class="img-pause me-3" height="30px">
-            <img src="assets/play.svg" alt="" class="me-3" height="30px">
+            <img src="assets/music.svg" alt="" class="img-play me-3 d-none" height="30px">
             <div>
                 <div>${track.title}</div>
-                <div>${track.author}</div>
+                <div class="text-secondary">${track.author}</div>
             </div>
-            <div class="ms-auto"><img src="assets/multimedia_play.svg" alt="" class="me-3" height="30px"> - ${track.time}</div>
+            <div class="time ms-auto"><img src="assets/multimedia_play.svg" alt="" class="me-3" height="30px"> - ${track.time}</div>
             <audio class="audio" src="${track.src}"></audio>
         </li>
         `;
@@ -52,28 +52,60 @@ if (!album) {
         let tracks = album.tracks;
         for (let i = 0; i < trackNodes.length; i++) { 
             // Один элемент
-            let node = trackNodes[i];   
+            let node = trackNodes[i];
+            let timeNode = node.querySelector(`.time`);
             // Тег аудио внутри этого элемента
-            let audio = node.querySelector(`.audio`); 
+            let audio = node.querySelector(`.audio`);
             let track = tracks[i];
             // продолжи самостоятельно
-        
+            let imgPause = node.querySelector(`.img-pause`);
+            let imgPlay = node.querySelector(`.img-play`);
+
             node.addEventListener(`click`, function () {
                 // Если трек сейчас играет...
                 if (track.isPlaying) {
                     track.isPlaying = false;
                     // Поставить на паузу
                     audio.pause();
-    
+                    imgPause.classList.remove(`d-none`);
+                    imgPlay.classList.add(`d-none`);
                 // Если трек сейчас не играет...
                 } else {
                     track.isPlaying = true;
                     // Включить проигрывание
                     audio.play();
+                    imgPause.classList.add(`d-none`);
+                    imgPlay.classList.remove(`d-none`);
+                    updateProgress();
                 }
             });
+
+            function updateProgress() {
+                // Нарисовать актуальное время
+                let time = getTime(audio.currentTime);
+                if (time != getTime(audio.currentTime)) {
+                    timeNode.innerHTML = getTime(audio.currentTime);
+                }
+                // Нужно ли вызвать её ещё раз?
+                if (track.isPlaying) {
+                    requestAnimationFrame(updateProgress);
+                }
+              }
         }
     }
 
     setupAudio();
+
+    function getTime(time) {
+        let currentSeconds = Math.floor(time);
+        let minutes = Math.floor(currentSeconds / 60);
+        let seconds = Math.floor(currentSeconds % 60);
+        if (minutes < 10) {
+            minutes = `0` + minutes;
+        }
+        if (seconds < 10) {
+            seconds = `0` + seconds;
+        }
+        return `${minutes}:${seconds}`;
+    }
 }
